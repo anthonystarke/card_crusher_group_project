113ad18d8aa773f1f9906e2d0381f29fadb3255e
EnigmaticGale <hamishwhytehw@gmail.com>
2019-03-09 23:44:52 +0000

    added simple phase order with functions to change phase, start game and override phase

--- client/src/models/TurnPhase_model.js
+++ client/src/models/TurnPhase_model.js
@@ -2,13 +2,30 @@
 
 const TurnPhaseModel = function() {
 
-  this.phaseOrder = {
+  this.currentPhase = null
 
+  this.phaseOrder = {
+    start: "draw",
+    draw: "play1",
+    play1: "battle",
+    battle: "play2",
+    play2: "end",
+    end: "start"
     };
 
 };
 
+TurnPhaseModel.prototype.startGame = function () {
+  this.currentPhase === "start";
+};
 
+TurnPhaseModel.prototype.nextPhase = function () {
+  this.currentPhase === this.phaseOrder[this.currentPhase];
+};
+
+TurnPhaseModel.prototype.overridePhase = function (chosenPhase) {
+  this.currentPhase === chosenPhase;
+};
 
 
 
