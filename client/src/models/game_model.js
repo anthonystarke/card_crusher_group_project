8aa34960946e489ad665642e3c943d7e84b416b6
Pauline Rudge <paulinerudge@me.com>
2019-03-12 09:49:02 +0000

    changed minor bug

--- client/src/models/game_model.js
+++ client/src/models/game_model.js
@@ -17,7 +17,7 @@ const GameModel = function(){
   this.flipCoin(this.player1,this.player2); //decide whos turn it is
   console.log(this.player1.getMyTurn(),this.player2.getMyTurn());
 
-  const nextTurn = document.querySelector('#next-turn');
+  const nextTurn = document.querySelector('#next-turn-wrapper');
   nextTurn.addEventListener('click',(evt) => {
 
     this.changeTurns();
@@ -50,7 +50,7 @@ GameModel.prototype.changeTurns = function () {
 
 GameModel.prototype.bindEvents = function () {
   PubSub.subscribe('GameView:Start-Game',(evt)=>{
-  
+
     this.publishData(this.player1,this.player2);
     this.mainGameLoop(this.player1,this.player2)
     // setInterval(() => {this.mainGameLoop(this.player1,this.player2)},250);
